29b4497723f9ccdaa0f9afe9d40f432536bcd0f2
radhasatsangi <82761211+radhasatsangi@users.noreply.github.com>
2021-07-30 20:28:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+canvas=document.getElementById("mycanvas");
+  ctx=canvas.getContext("2d");
+  
+  ctx.beginPath();
+  ctx.strokeStyle="Bule";
+  ctx.lineWidth=2;
+  ctx.arc(200,200,40,0,2*Math.PI);
+  ctx.stroke();
+  
+  
+  ctx.beginPath();
+  ctx.strokeStyle="Black";
+  ctx.lineWidth=2;
+  ctx.arc(200,200,40,0,2*Math.PI);
+  ctx.stroke();
+
+  ctx.beginPath();
+  ctx.strokeStyle="Red";
+  ctx.lineWidth=2;
+  ctx.arc(200,200,40,0,2*Math.PI);
+  ctx.stroke();
+
+  
+  ctx.beginPath();
+  ctx.strokeStyle="Yellow";
+  ctx.lineWidth=2;
+  ctx.arc(200,200,40,0,2*Math.PI);
+  ctx.stroke();
+
+  
+  ctx.beginPath();
+  ctx.strokeStyle="Green";
+  ctx.lineWidth=2;
+  ctx.arc(200,200,40,0,2*Math.PI);
+  ctx.stroke();
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+ 
+  }
